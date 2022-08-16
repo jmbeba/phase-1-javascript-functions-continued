5d781d0994adca58ec070dcf9c1e0c5ed284136c
John Johnson Mbeba <john.mbeba@student.moringaschool.com>
2022-08-16 12:16:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(act = "go to the office"){
+    return `This Monday, I will ${act}.`
+}
+
+function wrapAdjective(inp){
+    const result = function(adj = "special"){
+        return `You are ${inp}${adj}${inp}!`;
+    }
+
+    return result;
+}
